c16f87c96eb0379d9254922b38afb5efc568d7af
chenjunhong <alal1995@hotmail.com>
2019-12-26 10:55:06 +0800

    add: at defind logic

--- src/messages/dingding/impl/text/text.go
+++ src/messages/dingding/impl/text/text.go
@@ -2,6 +2,7 @@ package text
 
 import (
 	"github.com/ray1888/self-defined-dingbot/src/messages/dingding/impl"
+	"strings"
 )
 
 type Text struct {
@@ -23,8 +24,34 @@ func Init(content string) *TextMsg {
 	msg := new(TextMsg)
 	msg.MsgType.MsgType = impl.TextType
 	msg.Text.Content = content
-	// TODO just for testing @function
-	//msg.At.AtMobiles = []string{"13332889767"}
-	//msg.At.IsAtAll = true
+	atPersonNames := getAtPersonInContent(content)
+	if atPersonNames == "all" {
+		msg.SetAtAll()
+	} else {
+		msg.SetAtMobilesByNameList(atPersonNames)
+	}
 	return msg
+}
+
+func (t *TextMsg) SetAtAll() {
+	t.At.IsAtAll = true
+}
+
+func (t *TextMsg) SetAtMobilesByNameList(name string) {
+	// This is limit by gitlab can't assign assignee more than two person
+	t.At.AtMobiles = []string{name}
+}
+
+func getAtPersonInContent(content string) string {
+	if content == "" {
+		return content
+	}
+	splitContent := strings.Split(content, "\n")
+	// TODO use const instead
+	if splitContent[len(splitContent)-1] != "merge_request" {
+		return ""
+	}
+	atUsersLine := strings.Split(splitContent[len(splitContent)-2], ":")
+	atUser := strings.TrimSpace(atUsersLine[len(atUsersLine)-1])
+	return atUser
 }
--- src/test/client/client_test.go
+++ src/test/client/client_test.go
@@ -1,12 +1,12 @@
 package client
 
 import (
-	dingding "github.com/ray1888/self-defined-dingbot/src/clientsdk/client/impl/dingding"
 	"testing"
+
+	dingding "github.com/ray1888/self-defined-dingbot/src/clientsdk/client/impl/dingding"
 )
 
 func TestClientSendTextMsg(t *testing.T) {
-	// TODO to send
 	type Test struct {
 		Name   string
 		Input  string
@@ -19,11 +19,11 @@ func TestClientSendTextMsg(t *testing.T) {
 			"commit-messages:\n" +
 			"b6568db1b: Update Catalan translation to e38cb41.\n" +
 			"da1560886: fixed readme\n" +
-			"项目：Diaspora",
+			"项目：Diaspora" +
+			"Type: Push",
 			Output: ""},
-		{Name: "test MergeRequest Message",
-			Input:
-			"gitlab\n"+
+		{Name: "test MergeRequest Message BysinglePerson",
+			Input: "gitlab\n" +
 				"Administrator opened the merge request\n" +
 				"合入方向: ms-viewport -> master\n" +
 				"last_commit_msg:\n" +
@@ -33,8 +33,24 @@ func TestClientSendTextMsg(t *testing.T) {
 				"项目：Awesome Project\n" +
 				"创建时间：2013-12-03T17:15:43Z\n" +
 				"更新时间：2013-12-03T17:15:43Z\n" +
-				"链接: http://example.com/diaspora/merge_requests/1\n"+
-				"代码审阅人：@User1\n",
+				"链接: http://example.com/diaspora/merge_requests/1\n" +
+				"代码审阅人：13332889767\n" +
+				"Type: merge_request",
+			Output: ""},
+		{Name: "test MergeRequest Message At all",
+			Input: "gitlab\n" +
+				"Administrator opened the merge request\n" +
+				"合入方向: ms-viewport -> master\n" +
+				"last_commit_msg:\n" +
+				"da1560886d: fixed readme\n" +
+				"状态: unchecked\n" +
+				"标题: MS-Viewport\n" +
+				"项目：Awesome Project\n" +
+				"创建时间：2013-12-03T17:15:43Z\n" +
+				"更新时间：2013-12-03T17:15:43Z\n" +
+				"链接: http://example.com/diaspora/merge_requests/1\n" +
+				"代码审阅人：all\n" +
+				"Type: merge_request",
 			Output: ""},
 	}
 	client := dingding.Init()
@@ -43,4 +59,3 @@ func TestClientSendTextMsg(t *testing.T) {
 	}
 
 }
-
